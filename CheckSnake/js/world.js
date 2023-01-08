e07333e0c8bd7284ea374dcd551c2a6992a0128e
So Say Linn <94674631+Boixzz@users.noreply.github.com>
2023-01-08 23:50:44 +0630

    Add files via upload

---
+++ CheckSnake/js/world.js
@@ -0,0 +1,20 @@
+// Generating the World.
+const generateWorld = () => {
+    for (let rowIndex = 0; rowIndex < worldSize; rowIndex++) {
+        const row = document.createElement('div');
+    
+        row.classList.add('row');
+        
+        for (let columnIndex = 0; columnIndex < worldSize; columnIndex++) {
+            const input = document.createElement('input');
+    
+            input.type = 'checkbox';
+    
+            row.appendChild(input);
+        }
+    
+        document.querySelector('.world').appendChild(row);
+    }
+}
+
+generateWorld();
